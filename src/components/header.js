17e567c95c59610bc6253c559dd21da19dde6728
csslick <digitone@naver.com>
2019-09-07 23:57:32 +0900

    111

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ const Header = `
         <div class="jumbotron container bg-secondary">
             <h1>${title}</h1>
             <!-- <h2 class="text-primary">http://192.168.0.114/web0809</h2> -->
-            <h2>http://simsonart.dothome.co.kr/web/</h2>
+            <h2>https://csslick.github.io/web-study/</h2>
         </div>
     </header>
 `;
