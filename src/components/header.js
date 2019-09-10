9011100a4c2147647d68d432185c40510f606d02
web <digiton@naver.com>
2019-09-10 16:19:36 +0900

    111

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ const Header = `
     <div class="jumbotron container bg-dark">
         <h1>${title}</h1>
         <!-- <h2 class="text-primary">http://192.168.0.114/web0809</h2> -->
-        <h2>http://simsonart.dothome.co.kr/web/</h2>
+        <h2>https://csslick.github.io/web-study/</h2>
         <p><a class="btn btn-primary" href="https://cafe.naver.com/csslick">네이버 학습카페 바로가기</a></p>
     </div>
 </header>
